33e3a5ae54dc2ad8f9f50386b01a111b474a7e4d
MohammadRadja <radja.amri28@gmail.com>
2025-05-28 17:31:35 +0700

    feat(Routes): Update Routes

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ Route::get('/', function () {
     return redirect()->route('users.index');
 })->middleware('auth');
 
-
 // Login / Logout
 Route::get('/login', function () {
     return view('auth.login');
@@ -26,14 +25,9 @@ Route::post('/logout', function () {
     return redirect()->route('login');
 })->name('logout');
 
-// Semua route langsung bisa diakses tanpa login dan role middleware
 Route::middleware(['auth'])->group(function () {
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-    Route::post('/users', [UserController::class, 'store'])->name('users.store');
-    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::resource('users', UserController::class)->except(['show']);
+
     Route::get('/users/export', [UserController::class, 'export'])->name('users.export');
     Route::get('/exports', [UserController::class, 'showExport'])->name('exports.index');
     Route::get('/exports/download/{id}', [UserController::class, 'downloadExport'])->name('exports.download');
